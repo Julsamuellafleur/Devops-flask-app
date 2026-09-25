caa9e54e3f4563b0908f0595559b21b2489d77e4
Julien lafleur <D23125136@mytudublin.ie>
2026-09-25 11:53:02 +0100

    change greeting on main

--- hellhello.py
+++ hellhello.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return '''
-    <h1>Hello, World, I am a Flask app!</h1>
+    <h1>This is another string </h1>
     <p><a href="/about">About</a></p>
     <p><a href="/contact">Contact</a></p>
     '''
