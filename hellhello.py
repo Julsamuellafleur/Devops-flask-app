0db31e8ddc59130fecf5ceee0f9a638f0c1bab0c
Julien lafleur <D23125136@mytudublin.ie>
2026-09-25 11:47:57 +0100

    Add contact page

--- hellhello.py
+++ hellhello.py
@@ -1,8 +1,33 @@
+
+
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def say_hello():
-    return '<p>Hello, World, I am a Flask app!</p>'
 
+@app.route('/')
+def home():
+    return '''
+    <h1>Hello, World, I am a Flask app!</h1>
+    <p><a href="/about">About</a></p>
+    <p><a href="/contact">Contact</a></p>
+    '''
+
+
+@app.route('/about')
+def about():
+    return '''
+    <h1>About</h1>
+    <p>This is my DevOps Flask application.</p>
+    <p><a href="https://flask.palletsprojects.com/">Flask website</a></p>
+    '''
+
+
+@app.route('/contact')
+def contact():
+    return '''
+    <h1>Contact</h1>
+    <p>Email: D23125136@mytudublin.ie</p>
+    <p><a href="/">Back to home</a></p>
+    '''
